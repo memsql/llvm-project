888d7515aa264915a8c3f46cafd29f2eb9f54534
Eric(Yongzhu) Li <eli@memsql.com>
2020-11-12 13:50:41 -0800

    TLS patch for singlestore codegen.
    Summary:
    
    Author: Volodymyr Verovkin (vverovkin@)
    
    This diff was derived from parts of http://reviews.llvm.org/D8815,
    and it adds support for the local-exec
    TLS model when using the `Static` reloc model.
    
    **Docs impact**:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    JIRA Issues:

--- llvm/lib/ExecutionEngine/RuntimeDyld/RuntimeDyld.cpp
+++ llvm/lib/ExecutionEngine/RuntimeDyld/RuntimeDyld.cpp
@@ -128,6 +128,8 @@ void RuntimeDyldImpl::resolveRelocations() {
                  dumpSectionMemory(Sections[i], "before relocations"););
 
   // First, resolve relocations associated with external symbols.
+  if (TLSResolver)
+    resolveExternalTLSSymbols();
   if (auto Err = resolveExternalSymbols()) {
     HasError = true;
     ErrorStr = toString(std::move(Err));
@@ -930,16 +932,23 @@ void RuntimeDyldImpl::addRelocationForSection(const RelocationEntry &RE,
 }
 
 void RuntimeDyldImpl::addRelocationForSymbol(const RelocationEntry &RE,
-                                             StringRef SymbolName) {
+                                             StringRef SymbolName,
+                                             bool isTLS) {
   // Relocation by symbol.  If the symbol is found in the global symbol table,
   // create an appropriate section relocation.  Otherwise, add it to
   // ExternalSymbolRelocations.
   RTDyldSymbolTable::const_iterator Loc = GlobalSymbolTable.find(SymbolName);
   if (Loc == GlobalSymbolTable.end()) {
-    ExternalSymbolRelocations[SymbolName].push_back(RE);
+    if (isTLS) {
+      ExternalTLSRelocations[SymbolName].push_back(RE);
+    } else {
+      ExternalSymbolRelocations[SymbolName].push_back(RE);
+    }
   } else {
     assert(!SymbolName.empty() &&
            "Empty symbol should not be in GlobalSymbolTable");
+    assert(!isTLS && "Declaring thread local variables in loaded objects "
+                     "is not yet supported.");
     // Copy the RE since we want to modify its addend.
     RelocationEntry RECopy = RE;
     const auto &SymInfo = Loc->second;
@@ -1090,6 +1099,9 @@ void RuntimeDyldImpl::resolveRelocationList(const RelocationList &Relocs,
 
 void RuntimeDyldImpl::applyExternalSymbolRelocations(
     const StringMap<JITEvaluatedSymbol> ExternalSymbolMap) {
+  if (TLSResolver)
+    resolveExternalTLSSymbols();
+
   for (auto &RelocKV : ExternalSymbolRelocations) {
     StringRef Name = RelocKV.first();
     RelocationList &Relocs = RelocKV.second;
@@ -1135,6 +1147,11 @@ void RuntimeDyldImpl::applyExternalSymbolRelocations(
         resolveRelocationList(Relocs, Addr);
       }
     }
+
+    // Resolving this symbol may have loaded additional modules whose TLS
+    // Relocations need to be resolved.
+    if (TLSResolver)
+      resolveExternalTLSSymbols();
   }
   ExternalSymbolRelocations.clear();
 }
@@ -1266,8 +1283,9 @@ void JITSymbolResolver::anchor() {}
 void LegacyJITSymbolResolver::anchor() {}
 
 RuntimeDyld::RuntimeDyld(RuntimeDyld::MemoryManager &MemMgr,
-                         JITSymbolResolver &Resolver)
-    : MemMgr(MemMgr), Resolver(Resolver) {
+                         JITSymbolResolver &Resolver,
+                         RuntimeDyld::TLSSymbolResolver *TLSResolver)
+    : MemMgr(MemMgr), Resolver(Resolver), TLSResolver(TLSResolver) {
   // FIXME: There's a potential issue lurking here if a single instance of
   // RuntimeDyld is used to load multiple objects.  The current implementation
   // associates a single memory manager with a RuntimeDyld instance.  Even
@@ -1296,8 +1314,11 @@ static std::unique_ptr<RuntimeDyldELF>
 createRuntimeDyldELF(Triple::ArchType Arch, RuntimeDyld::MemoryManager &MM,
                      JITSymbolResolver &Resolver, bool ProcessAllSections,
                      RuntimeDyld::NotifyStubEmittedFunction NotifyStubEmitted) {
+  LegacyJITSymbolResolver *SR = dynamic_cast<LegacyJITSymbolResolver*>(&Resolver);
+  assert(SR != nullptr);
+  RuntimeDyld::TLSSymbolResolver *TLSResolver = new TLSSymbolResolverGLibCELF(SR);
   std::unique_ptr<RuntimeDyldELF> Dyld =
-      RuntimeDyldELF::create(Arch, MM, Resolver);
+      RuntimeDyldELF::create(Arch, MM, Resolver, TLSResolver);
   Dyld->setProcessAllSections(ProcessAllSections);
   Dyld->setNotifyStubEmitted(std::move(NotifyStubEmitted));
   return Dyld;
